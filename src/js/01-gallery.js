e5bbff40c7deabb36351c6abad5b80441f821ac1
Michał Murawski <m.a.murawski@wp.pl>
2022-09-22 23:48:35 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,7 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+import * as basicLightbox from 'https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js'
+
+console.log(basicLightbox)
