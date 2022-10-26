5f36b03d00c89af84bb0ff8a1748e302099dccfd
Michał Murawski <m.a.murawski@wp.pl>
2022-10-27 00:36:15 +0200

    lightbox gallery add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,12 +2,13 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 // console.log("gallery:", galleryItems);
 
-const galleryParent = document.querySelector('.gallery');
+const galleryParent = document.querySelector(".gallery");
 // console.log(galleryParent);
 const markupGallery = addImageToMarkupGallery(galleryItems);
 // console.log(markupGallery);
 const galleryHtml = addGalleryToHtml(markupGallery, galleryParent);
 // console.log(galleryHtml);
+galleryParent.addEventListener("click", imageClick);
 
 function addImageToMarkupGallery(imagesTable) {
   return imagesTable
@@ -18,14 +19,30 @@ function addImageToMarkupGallery(imagesTable) {
                 <img
                    class="gallery__image"
                    src="${image.preview}"
+                   data-source="${image.original}"
                    alt="${image.description}"
                 />
             </a>
         </div>`
     )
-    .join('\r\n');
+    .join("\r\n");
 }
 
 function addGalleryToHtml(gallery, htmlElement) {
   htmlElement.innerHTML = gallery;
+}
+
+function imageClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const lightboxDiv = document.createElement("div");
+  const lightboxImg = event.target.cloneNode(true);
+  lightboxImg.src = event.target.getAttribute("data-source");
+  lightboxDiv.append(lightboxImg);
+  const instance = basicLightbox.create(lightboxDiv);
+  instance.show();
 }
