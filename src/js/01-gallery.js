dc7112a20ed235eafe7ffe5469b5d1fcbca36331
Michał Murawski <m.a.murawski@wp.pl>
2022-10-26 22:06:36 +0200

    task 01 - add gallery to html

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// console.log("gallery:", galleryItems);
 
-console.log(galleryItems);
+const galleryParent = document.querySelector('.gallery');
+// console.log(galleryParent);
+const markupGallery = addImageToMarkupGallery(galleryItems);
+// console.log(markupGallery);
+const galleryHtml = addGalleryToHtml(markupGallery, galleryParent);
+// console.log(galleryHtml);
 
-import * as basicLightbox from 'https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js'
+function addImageToMarkupGallery(imagesTable) {
+  return imagesTable
+    .map(
+      (image) =>
+        `<div class="gallery__item">
+            <a class="gallery__link" href="${image.original}">
+                <img
+                   class="gallery__image"
+                   src="${image.preview}"
+                   alt="${image.description}"
+                />
+            </a>
+        </div>`
+    )
+    .join('\r\n');
+}
 
-console.log(basicLightbox)
+function addGalleryToHtml(gallery, htmlElement) {
+  htmlElement.innerHTML = gallery;
+}
