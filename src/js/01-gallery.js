14382b9615eae7527e45635ce867d2afee736d01
Michał Murawski <109108678+AMichalMurawski@users.noreply.github.com>
2022-10-27 10:44:12 +0200

    exit lightbox with escape
    Add in github.io

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -46,3 +46,19 @@ function imageClick(event) {
   const instance = basicLightbox.create(lightboxDiv);
   instance.show();
 }
+
+onShow: (instance) => {
+	// Close when hitting escape.
+	document.onkeydown = function(evt) {
+		evt = evt || window.event;
+		var isEscape = false;
+		if ( "key" in evt ) {
+			isEscape = ( evt.key === "Escape" || evt.key === "Esc" );
+		} else {
+			isEscape = ( evt.keyCode === 27 );
+		}
+		if ( isEscape ) {
+			instance.close();
+		}
+	};
+},
