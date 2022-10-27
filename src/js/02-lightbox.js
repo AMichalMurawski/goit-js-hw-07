430df61c431f406a5e5a0328436fe51e64c2fae3
Michał Murawski <m.a.murawski@wp.pl>
2022-10-27 22:05:38 +0200

    add gallery to html
    copy code from 01-gallery.js

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryParent = document.querySelector(".gallery");
+const markupGallery = addImageToMarkupGallery(galleryItems);
+const galleryHtml = addGalleryToHtml(markupGallery, galleryParent);
+galleryParent.addEventListener("click", imageClick);
+
+function addImageToMarkupGallery(imagesTable) {
+  return imagesTable
+    .map(
+      (image) =>
+        `<div class="gallery__item">
+            <a class="gallery__link" href="${image.original}">
+                <img
+                   class="gallery__image"
+                   src="${image.preview}"
+                   data-source="${image.original}"
+                   alt="${image.description}"
+                />
+            </a>
+        </div>`
+    )
+    .join("\r\n");
+}
+
+function addGalleryToHtml(gallery, htmlElement) {
+  htmlElement.innerHTML = gallery;
+}
+
+function imageClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+}
